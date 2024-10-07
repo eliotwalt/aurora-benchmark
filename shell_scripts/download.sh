3d14b8d268b531140669ded692f94f937a6af2ea
eliotwalt <walt.eliot@hotmail.com>
2024-10-07 18:31:08 +0200

    slurm parallelisation download

--- shell_scripts/download.sh
+++ shell_scripts/download.sh
@@ -13,7 +13,7 @@ num_jobs=$(python ./py_scripts/task_array.py $DOWNLOAD_CONFIG)
 ./shell_scripts/load_env.sh --cpu
 
 # download data with python script
-sbatch --cpus-per-task=1 --mem=32G --time=6:00:00 --output=./logs/%A_%a.out \
+sbatch --cpus-per-task=1 --mem=32G --time=6:00:00 --output=./logs/download/%A_%a.out \
     --error=./logs/%A_%a.err --job-name=dl_era5_wb2 --partition=staging \
     --array=0-$((num_jobs-1))%1 \
     --wrap="python ./py_scripts/download.py --download_config $DOWNLOAD_CONFIG --host_config $HOST_CONFIG"
