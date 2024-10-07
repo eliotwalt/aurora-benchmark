a3bfb0d35cb7fb04b64552ccbe6ce730fb16d27a
eliotwalt <walt.eliot@hotmail.com>
2024-10-07 18:28:43 +0200

    slurm parallelisation download

--- shell_scripts/download.sh
+++ shell_scripts/download.sh
@@ -3,18 +3,8 @@
 DOWNLOAD_CONFIG=./configs/download_era5_wb2_1979-2022-6h-1444x721.yaml
 HOST_CONFIG=./configs/snellius.yaml
 
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=32G
-#SBATCH --time=6:00:00
-#SBATCH --output=./logs/%A_%a.out
-#SBATCH --error=./logs/%A_%a.err
-#SBATCH --job-name=dl_era5_wb2
-#SBATCH --partition=staging
-
-# Update the array size dynamically
+# compute num jobs
 num_jobs=$(python ./py_scripts/task_array.py $DOWNLOAD_CONFIG)
-scontrol update JobId=$SLURM_JOB_ID ArrayTaskLimit=$num_jobs
 
 # load modules
 ./env/modules_cpu.sh
@@ -23,5 +13,7 @@ scontrol update JobId=$SLURM_JOB_ID ArrayTaskLimit=$num_jobs
 ./shell_scripts/activate_cpu.sh --cpu
 
 # download data with python script
-python ./py_scripts/download.py --download_config $DOWNLOAD_CONFIG \
-                                --host_config $HOST_CONFIG
+sbatch --cpus-per-task=1 --mem=32G --time=6:00:00 --output=./logs/%A_%a.out \
+    --error=./logs/%A_%a.err --job-name=dl_era5_wb2 --partition=staging \
+    --array=0-$((num_jobs-1))%1 \
+    --wrap="python ./py_scripts/download.py --download_config $DOWNLOAD_CONFIG --host_config $HOST_CONFIG"
