c5cb0be8d7d049fcb690bc8bfdce42945bfa84d1
eliotwalt <walt.eliot@hotmail.com>
2024-10-08 18:25:15 +0200

    transfer

--- shell_scripts/download.sh
+++ shell_scripts/download.sh
@@ -11,7 +11,7 @@ source ./env/venv_cpu/bin/activate
 num_jobs=$(python ./py_scripts/download_task_array.py $DOWNLOAD_CONFIG)
 
 # download data with python script
-sbatch --cpus-per-task=1 --mem=32G --time=6:00:00 --output=./logs/download/%A_%a.out \
+sbatch --cpus-per-task=2 --mem=32G --time=6:00:00 --output=./logs/download/%A_%a.out \
     --error=./logs/download/%A_%a.out --job-name=dl_era5_wb2 --partition=staging \
     --array=0-$((num_jobs-1))%1 \
     --wrap="./env/modules_cpu.sh && source ./env/venv_cpu/bin/activate  && python ./py_scripts/download.py --download_config $DOWNLOAD_CONFIG --host_config $HOST_CONFIG"
