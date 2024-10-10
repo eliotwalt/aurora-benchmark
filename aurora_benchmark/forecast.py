a5c13474833934a7d2fea382cf37306c6a631e0d
eliotwalt <walt.eliot@hotmail.com>
2024-10-10 17:40:00 +0200

    forecast debug

--- aurora_benchmark/forecast.py
+++ aurora_benchmark/forecast.py
@@ -89,26 +89,20 @@ def aurora_forecast(
         raise NotImplementedError("Replacement variables not yet implemented.")
     
     # dask
-    chunks = None #{"time": 12*batch_size, "latitude": 721, "longitude": 1440}
+    chunks = {"time": 12*batch_size, "latitude": 721, "longitude": 1440}
     
     # load xr data
     verbose_print(verbose, "Reading data ...")
     surface_ds = xr.merge(
-        [xr.open_dataset(
-            path, engine="netcdf4",
-            chunks=chunks) 
+        [xr.open_dataset(path, engine="netcdf4").chunk(chunks=chunks) 
          for path in era5_surface_paths],
     ).rename(AURORA_VARAIBLE_RENAMES["surface"])
     atmospheric_ds = xr.merge(
-        [xr.open_dataset(
-            path, engine="netcdf4",
-            chunks=chunks) 
+        [xr.open_dataset(path, engine="netcdf4").chunk(chunks=chunks) 
          for path in era5_atmospheric_paths],
     ).rename(AURORA_VARAIBLE_RENAMES["atmospheric"])
     static_ds = xr.merge(
-        [xr.open_dataset(
-            path, engine="netcdf4",
-            chunks=chunks) 
+        [xr.open_dataset(path, engine="netcdf4").chunk(chunks=chunks) 
          for path in era5_static_paths],
     ).rename(AURORA_VARAIBLE_RENAMES["static"])
     
