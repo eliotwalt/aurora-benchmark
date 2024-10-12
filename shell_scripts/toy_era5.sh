49aceede34213e8cc6082061d8e1770c2a9f1958
eliotwalt <walt.eliot@hotmail.com>
2024-10-12 09:57:26 +0200

    toy era

--- shell_scripts/toy_era5.sh
+++ shell_scripts/toy_era5.sh
@@ -12,12 +12,13 @@ module load 2023
 module load CDO/2.2.2-gompi-2023a
 
 # find all the variables as /projects/prjs0981/ewalt/aurora_benchmark/data/era5_wb2/2021-2022-6h-1444x721/${variable}_2021-2022-6h-1440x721.nc
-variables="msl q sst t t2m tp u10 u v10 v z"
+variables=(msl q sst t t2m tp u10 u v10 v z)
 
 mkdir -p tmp_data
 
 variable=${variables[${SLURM_ARRAY_TASK_ID}]}
 echo "Processing ${variable}"
+
 input=/projects/prjs0981/ewalt/aurora_benchmark/data/era5_wb2/2021-2022-6h-1444x721/${variable}_2021-2022-6h-1440x721.nc
 output=tmp_data/${variable}-2021-2022-1d-360x180.nc
 cdo -f nc -b F32 -daymean -remapbil,r360x180 $input $output
